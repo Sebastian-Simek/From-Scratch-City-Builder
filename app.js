03f797d2086f074f274a2fe35171f94fe6b790ca
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-04-28 14:13:02 -0700

    added dropdown eventlisteners

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-// import functions and grab DOM elements
-
-// let state
+const climateDropdownEl = document.getElementById('climate-dropdown');
+const designDropdownEl = document.getElementById('design-dropdown');
+const cityWeatherEl = document.getElementById('weather');
+const cityHousesEl = document.getElementById('houses');
+// let citySlogans = [];
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+climateDropdownEl.addEventListener('change', () =>{ 
+    cityWeatherEl.style.backgroundImage = `url(../assets/${climateDropdownEl.value}-climate.png)`;
+});
+
+designDropdownEl.addEventListener('change', () => {
+    cityHousesEl.style.backgroundImage = `url(../assets/${designDropdownEl.value}-architecture.png)`;
+});
