575a4fdf45f3c5de7169284c3a9b68c9c04aab77
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-04-28 14:25:45 -0700

    added eventlistner and pushed value to array

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const climateDropdownEl = document.getElementById('climate-dropdown');
 const designDropdownEl = document.getElementById('design-dropdown');
 const cityWeatherEl = document.getElementById('weather');
 const cityHousesEl = document.getElementById('houses');
-// let citySlogans = [];
+const citySloganInput = document.getElementById('slogans');
+const myButton = document.getElementById('my-button');
+let citySlogansArray = [];
 
 // set event listeners 
   // get user input
@@ -16,3 +18,7 @@ climateDropdownEl.addEventListener('change', () =>{
 designDropdownEl.addEventListener('change', () => {
     cityHousesEl.style.backgroundImage = `url(../assets/${designDropdownEl.value}-architecture.png)`;
 });
+myButton.addEventListener('click', () => {
+    citySlogansArray.push(citySloganInput.value);
+});
+console.log(citySlogansArray);
