401acb1a1ffad9831a642d955c6900bb14f836c1
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-04-28 16:35:34 -0700

    added dropdown count

--- app.js
+++ app.js
@@ -5,26 +5,41 @@ const cityHousesEl = document.getElementById('houses');
 const citySloganInput = document.getElementById('slogans');
 const myButton = document.getElementById('my-button');
 const newElement = document.getElementById('slogan-output');
+const countryDropdownEl = document.getElementById('country-dropdown');
+const cityCountry = document.getElementById('countries');
+const numberOfClicks = document.getElementById('number-of-drops');
 
 let citySlogansArray = [];
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+let climateCount = 0;
+let designCount = 0;
+let flagCount = 0;
 
 climateDropdownEl.addEventListener('change', () =>{ 
     cityWeatherEl.style.backgroundImage = `url(../assets/${climateDropdownEl.value}-climate.png)`;
+    climateCount++;
+    displayStats();
 });
 
 designDropdownEl.addEventListener('change', () => {
     cityHousesEl.style.backgroundImage = `url(../assets/${designDropdownEl.value}-architecture.png)`;
+    designCount++;
+    displayStats();
 });
+
+countryDropdownEl.addEventListener('change', () => {
+    cityCountry.style.backgroundImage = `url(../assets/${countryDropdownEl.value}-flag.png)`;
+    flagCount++;
+    displayStats();
+});
+
 myButton.addEventListener('click', () => {
     citySlogansArray.push(citySloganInput.value);
     displayCitySlogans();
 });
-console.log(citySlogansArray);
+
+const displayStats = () => {
+    numberOfClicks.textContent = `You have changed the climate ${climateCount} time(s), the design ${designCount} time(s), and the country ${flagCount} time(s)`;
+};
 
 const displayCitySlogans = () => {
     newElement.textContent = '';
